b0a4b06623417217ce1364a023f9208bc8daa364
A.M <andrej2000aa@gmail.com>
2022-10-28 12:44:36 +0200

    ubaceno vreme

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+import requests
+import time
+from vreme import prognoza
 
 app = Flask(__name__)
 
@@ -6,6 +9,12 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('home.html')
+
+
+@app.route('/vreme')
+def vreme():
+    return prognoza()
+
 
 
 if __name__ == '__main__':
