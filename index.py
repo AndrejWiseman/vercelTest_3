ee17745966327f895f794714b185577ba90352dd
A.M <andrej2000aa@gmail.com>
2022-12-01 15:18:44 +0100

    izbrisani vicevi

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for
 import requests
 import time
 from vreme import prognoza
-from vicevi import vic_random
+
 
 app = Flask(__name__)
 
@@ -16,9 +16,6 @@ def home():
 def vreme():
     return prognoza()
 
-# @app.route('/vicevi')
-# def vic():
-#     return vic_random()
 
 
 if __name__ == '__main__':
